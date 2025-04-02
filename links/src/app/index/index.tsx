0496c3b07d493af2f49617e0cb3fe4b65ab3c563
Rodolfo Ferreira <ref.rodolfo@gmail.com>
2025-04-01 21:00:50 -0300

    feat: adicionando componente customizado
    Add o componente nativo: Pressable
    Tem função igual do cursor pointer. É diferente do TouchableOpacity, pq esse tem efeito de opacidade tbm, o Pressable  somente de cursor pointer

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -2,6 +2,7 @@ import { View, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles"
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
+import { Category } from "@/components/category";
 
 export default function Index() {
   return (
@@ -19,6 +20,9 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+
+      <Category />
     </View>
   );
 } 
